379a0e723df3a1ef46faa816b6b72ad86842bad3
durgapatro15 <durgapatro2002@gmail.com>
2025-03-05 23:52:14 +0530

    Removed secrets

--- src/main/java/com/rent/rentavehicle/config/CorsConfig.java
+++ src/main/java/com/rent/rentavehicle/config/CorsConfig.java
@@ -11,11 +11,13 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") 
+                registry.addMapping("/**") 
                         .allowedOrigins("http://localhost:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+            
+            
                         
             }
         };
